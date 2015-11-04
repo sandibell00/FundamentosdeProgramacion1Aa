255067f3c71a1ec24e0db47be7d944184e1762dd
sandibell00 <sandibell00@gmail.com>
2015-11-03 20:53:13 -0600

    FUP1A

---
+++ While2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int n1;
+int n2;
+int main()
+{ 
+	printf("Dame un numero");
+	scanf("%d", &n1);
+	printf("Dame otro numero");
+	scanf("%d", &n2);
+	if (n1<=n2)
+
+	
+		while(n1<=n2)
+		{
+		printf("%d", n1);
+		n1=n1+1; }
+
+		else if(n2<=n1)
+	
+		
+	
+		while(n1<=n2)
+		{
+		printf("%d", n1);
+		n1=n1-1; }
+		
+	
+
+		return 0;
+	}
