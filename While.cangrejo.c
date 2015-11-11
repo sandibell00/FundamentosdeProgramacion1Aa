eef676bd26517962831731cda9e50655007a0d84
sandibell00 <sandibell00@gmail.com>
2015-11-10 20:46:19 -0600

    Revert "Evidencia 1"
    This reverts commit 592ff7ec7c216e3c8bc3bafb478ba09d6623d2a3.

--- While.cangrejo.c
+++ While.cangrejo.c
@@ -9,9 +9,9 @@
 	printf("Programa que calcula la distancia del oyo\n\n");
 	printf("Dame la profundidad del oyo\n");
 	scanf("%d", &distancia);
-	printf("Da los metros que a recorrido en el dia\n\n");
+	printf("Da los dias que a recorrido en la noche\n\n");
 	scanf("%d", &avance);
-	printf("Da los metros que recorrio en la noche\n");
+	printf("Da los dias que recorrio en la noche\n");
 	scanf("%d", &baj);
 	if(baj>=avance){
 		printf("y el cangrejito nunca podra salir :(");
